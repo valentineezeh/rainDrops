f8889f003310b63c77abde956c3b7a0c488f4209
valentineezeh <32917511+valentineezeh@users.noreply.github.com>
2017-10-27 11:09:50 +0100

    restructured the codes in main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
+/**
+ * @param {Number} num
+ * @returns {Number} num  
+ */
 
 const rainDrops = (num) => {
-  	let output = "";
-  	if (!Number(num)) return "invalid input"
-    if( num % 3 === 0) { output += "Pling" }
-    if (num % 5 === 0) { output += "Plang" }
-  	if (num % 7 === 0) { output += "Plong" }
-  	if (output.length === 0) { return num }
+  let output = "";
+  if (!Number(num)) {
+	  return "invalid input"
+	} if( num % 3 === 0) { 
+			output += "Pling" 
+		} if (num % 5 === 0) { 
+			output += "Plang" 
+		} if (num % 7 === 0) { 
+		  output += "Plong" 
+		} if (output.length === 0) {
+			return num 
+		}
   		return output;
-  };
+};
 
 export default rainDrops;
